d03a28af4e9e6aa38cebc9047eddeb5fa5cdc46f
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2024-12-17 20:32:23 -0600

    Initial Commit

---
+++ MoviesClean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesClean
+//
+//  Created by Marco Alonso Rodriguez on 17/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesClean/MoviesCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCleanApp.swift
+//  MoviesClean
+//
+//  Created by Marco Alonso Rodriguez on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
